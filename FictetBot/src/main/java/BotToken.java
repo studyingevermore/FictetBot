a44071805549ba8932fd5149f80352d7f05a9b4d
ykd755 <ykd755+git@my.utsa.edu>
2020-12-09 20:21:29 -0600

    Another try (Token error)

--- FictetBot/src/main/java/BotToken.java
+++ FictetBot/src/main/java/BotToken.java
@@ -1,5 +1,5 @@
 public class BotToken {
-    static String token = "Nzg2MzU5MDI5NDI4NzgxMDg2.X9FP6g.YYS9_GDyhuwz8ql7KhRx-eojCQI";
+    static String token = "Nzg2MzU5MDI5NDI4NzgxMDg2.X9FP6g.UM1XJFEJ8INr0lQM739HbFZZN-0";
     public static String getToken() {
         return token;
     }
