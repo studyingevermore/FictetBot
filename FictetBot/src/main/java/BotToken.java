10effa5368a655ef9cb55dddf2c621eac0821679
ykd755 <ykd755+git@my.utsa.edu>
2020-12-09 20:17:47 -0600

    One more try (Token error)

--- FictetBot/src/main/java/BotToken.java
+++ FictetBot/src/main/java/BotToken.java
@@ -1,5 +1,5 @@
 public class BotToken {
-    static String token = "Nzg2MzU5MDI5NDI4NzgxMDg2.X9FP6g.lTBIZl3FlqkelY5oSRywrC7JBVc";
+    static String token = "Nzg2MzU5MDI5NDI4NzgxMDg2.X9FP6g.YYS9_GDyhuwz8ql7KhRx-eojCQI";
     public static String getToken() {
         return token;
     }
