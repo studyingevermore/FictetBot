5e01e8b3ffbf4b4c5be39c9c90a279a5c08031f1
ykd755 <ykd755+git@my.utsa.edu>
2020-12-09 20:15:43 -0600

    Recommit (Token error)

--- FictetBot/src/main/java/BotToken.java
+++ FictetBot/src/main/java/BotToken.java
@@ -1,5 +1,5 @@
 public class BotToken {
-    static String token = "Nzg2MzU5MDI5NDI4NzgxMDg2.X9FP6g.MBgdbF4mIqSUhTlw0UPsLVcLHeg";
+    static String token = "Nzg2MzU5MDI5NDI4NzgxMDg2.X9FP6g.lTBIZl3FlqkelY5oSRywrC7JBVc";
     public static String getToken() {
         return token;
     }
