0903887c6223c738021cb867a78c3a9258d79696
ykd755 <ykd755+git@my.utsa.edu>
2020-12-09 20:22:53 -0600

    Delete BotToken.java

--- FictetBot/src/main/java/BotToken.java
+++
@@ -1,8 +0,0 @@
-public class BotToken {
-    static String token = "Nzg2MzU5MDI5NDI4NzgxMDg2.X9FP6g.UM1XJFEJ8INr0lQM739HbFZZN-0";
-    public static String getToken() {
-        return token;
-    }
-
-
-}
